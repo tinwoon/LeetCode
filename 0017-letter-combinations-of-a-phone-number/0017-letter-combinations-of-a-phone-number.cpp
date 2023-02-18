8c54e67d8b54289509191c47a8707d4b30496040
Oh jiwon <tinwoon21@gmail.com>
2023-02-18 18:48:17 +0900

    Time: 4 ms (35.28%), Space: 6.7 MB (32.48%) - LeetHub

--- 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
+++ 0017-letter-combinations-of-a-phone-number/0017-letter-combinations-of-a-phone-number.cpp
@@ -3,22 +3,24 @@ public:
     std::map<char, std::string> mp = { {2, "abc"}, {3, "def"}, {4, "ghi"}, {5, "jkl"}, {6, "mno"}, {7, "pqrs"}, {8,"tuv"}, {9, "wxyz"}};
     
     std::vector<std::string>  ans = {};
+    std::string dgt;
     
-    void calculate(string& digits, int level, std::string val){
-        if(level == digits.size()){
+    void calculate(int level, std::string val){
+        if(level == dgt.size()){
             if(val.size()) ans.emplace_back(val);
             return;
         }
         //nums에 해당하는 알파벳들
-        std::string nums = mp[digits[level] - '0'];
+        std::string nums = mp[dgt[level] - '0'];
         
         for(int k=0; k < nums.size(); k++){
-            calculate(digits, level+1, val + nums[k]);
+            calculate(level+1, val + nums[k]);
         }
     }
     
-    vector<string> letterCombinations(string digits) {
-        calculate(digits, 0, "");
+    vector<string> letterCombinations(string digits){
+        dgt = digits;
+        calculate(0, "");
         return ans;
     }
 };
