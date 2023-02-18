c6aa6696f88176a5bf89c154f3162d34a23bf8f7
Oh jiwon <tinwoon21@gmail.com>
2023-02-18 18:17:42 +0900

    Time: 51 ms (73.31%), Space: 10.1 MB (98.87%) - LeetHub

--- 0016-3sum-closest/0016-3sum-closest.cpp
+++ 0016-3sum-closest/0016-3sum-closest.cpp
@@ -1,27 +1,34 @@
 class Solution {
 public:
     int threeSumClosest(vector<int>& nums, int target) {
-        std::sort(nums.begin(), nums.end());
-        int diff = fabs(nums[0] + nums[1] + nums[2] - target);
-        int ans = nums[0] + nums[1] + nums[2];
-        
-        for(int i=0; i<nums.size(); i++){
-            for(int j= i + 1; j<nums.size(); j++){
-                int idx = j+1;
-                if(idx < nums.size()){
-                    int val = nums[i] + nums[j] + nums[idx];
-                    while(val <= target + diff){
-                        if((val > target - diff) && (val < target + diff)){
-                            diff = abs(val - target);
-                            ans = val;
-                        }
-                        if(idx + 1 < nums.size()){
-                            val += nums[idx + 1] - nums[idx++];
-                        }else break;
-                    }
-                }
+         sort(nums.begin(),nums.end());
+        int maxsum=1000000000;
+        for(int i=0;i<nums.size()-2;i++)
+        {
+          int sum=nums[i];
+          int j=i+1;
+          int k=nums.size()-1;
+          while(j<k)
+          {
+            sum=nums[i]+nums[j]+nums[k];
+            if(sum==target)
+            {
+                return target;
             }
+            if(abs(sum-target)<abs(maxsum-target))
+            {
+                maxsum=sum;
+            }
+            if(sum>target)
+            {
+                k--;
+            }
+            else if(sum<target)
+            {
+                j++;
+            }
+          }
         }
-        return ans;
+        return maxsum;
     }
 };
