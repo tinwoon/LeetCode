ae97e09666b4659a13a3e23c00743b9ee3a328f6
Oh jiwon <tinwoon21@gmail.com>
2023-05-13 17:55:56 +0900

    Time: 1107 ms (10.52%), Space: 53.5 MB (6.54%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    vector<int> gnums, visited;
+    
+    bool dfs(int idx){
+        if(idx == gnums.size() - 1) return true;
+        visited[idx] = true;
+        bool ret = false;
+        
+        for(int k = 1; k <= gnums[idx]; k++){
+            int n_idx = idx + k;
+            if(n_idx < gnums.size()){
+                if(visited[n_idx] == 0) ret = dfs(n_idx);
+                if(ret) return true;
+            }
+        }
+        return false;
+    }
+    
+    bool canJump(vector<int>& nums) {
+        gnums =  nums;
+        visited.assign(nums.size(), false);
+        
+        return nums.size() == 1 ? true : dfs(0);
+    }
+};
