0839b42adbad256a6533a5fa6fc2ac30992c4226
Oh jiwon <tinwoon21@gmail.com>
2023-07-02 14:21:27 +0900

    Time: 3 ms (60.44%), Space: 15.1 MB (37.35%) - LeetHub

--- 0078-subsets/0078-subsets.cpp
+++ 0078-subsets/0078-subsets.cpp
@@ -1,30 +1,28 @@
 class Solution {
 public:
     std::vector< std::vector<int> > ans;
-    std::vector<int> NUMS;
     
     //몇번째까지 체크했는지 안했는지 여부, 비트마스크 기준으로 idx 넣었을 시의 값
-    void calculate(int level, int idx){
+    void calculate(int level, int idx, std::vector<int>& nums){
         if(level == 0){
             std::vector<int> ret;
-            for(int k = 0; k < NUMS.size(); k++){
-                if(idx & (1 << k)) ret.emplace_back(NUMS[k]);
+            for(int k = 0; k < nums.size(); k++){
+                if(idx & (1 << k)) ret.emplace_back(nums[k]);
             }
             ans.emplace_back(ret);
             return;
         }
         
         //미추가
-        calculate(level - 1, idx);
+        calculate(level - 1, idx, nums);
         //추가
-        calculate(level - 1, idx | (1<<(NUMS.size() - level)));
+        calculate(level - 1, idx | (1<<(nums.size() - level)), nums);
 
 
     }
     
     vector<vector<int>> subsets(vector<int>& nums) {
-        NUMS = nums;
-        calculate(nums.size(), 0);
+        calculate(nums.size(), 0, nums);
         return ans;
     }
 };
