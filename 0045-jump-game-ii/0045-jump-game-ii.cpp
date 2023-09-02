831a3cd6006144d8d802a8fb1b57167d42283b7e
Oh jiwon <tinwoon21@gmail.com>
2023-09-02 14:56:59 +0900

    Time: 183 ms (37.22%), Space: 17.6 MB (18.49%) - LeetHub

---
+++ 0045-jump-game-ii/0045-jump-game-ii.cpp
@@ -0,0 +1,38 @@
+class Solution {
+public:
+    
+    
+    
+    int calculate(vector<int>& nums){
+        
+        std::queue< std::pair<int, int> > q;
+        q.emplace(std::make_pair(0,0));
+        
+        std::vector<bool> visited(nums.size(), false);
+        visited[0] = true;
+        
+        while(!q.empty()){
+            auto front = q.front();
+            q.pop();
+            
+            int idx = front.first;
+            int jump = front.second;
+            
+            if(idx == (nums.size() - 1)) return jump;
+            
+            for(int i = idx + 1; i <= idx + nums[idx]; i++){
+                if((i < nums.size()) && !visited[i]){
+                    visited[i] = true;
+                    q.emplace(std::make_pair(i, jump + 1));
+                }
+            }
+            
+        }
+        
+        return -1;
+    }
+    
+    int jump(vector<int>& nums) {
+        return calculate(nums);
+    }
+};
