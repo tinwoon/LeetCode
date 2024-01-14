67d236541b69d3c3390a3c6ea55b6b71c49ecdcd
Oh jiwon <tinwoon21@gmail.com>
2024-01-14 15:06:55 +0900

    Time: 4 ms (77.34%), Space: 9.4 MB (19.02%) - LeetHub

---
+++ 0120-triangle/0120-triangle.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    std::vector< std::vector<int> > dp;
+    std::vector< std::vector<int> > trngl;
+    
+    int calculate(int level, int j){
+        if(dp[level][j] != INT_MAX) return dp[level][j];
+        
+        if(j+1 >= trngl[level + 1].size()){
+            return dp[level][j] = calculate(level + 1, j) + trngl[level][j];
+        }
+        
+        return dp[level][j] = std::min(calculate(level + 1, j), calculate(level + 1, j+1)) + trngl[level][j];
+    }
+    
+    int minimumTotal(vector<vector<int>>& triangle) {
+        dp = trngl = triangle;
+        
+        for(int idx = 0; idx < dp.size() - 1; idx++){
+            std::fill(dp[idx].begin(), dp[idx].end(), INT_MAX);
+        }
+        
+        return calculate(0,0);
+    }
+};
