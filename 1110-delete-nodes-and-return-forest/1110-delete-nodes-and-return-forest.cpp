0b1c67fc86aa9e59909c8958e6cbadac9ac9dc6f
Oh jiwon <tinwoon21@gmail.com>
2024-12-01 14:37:19 +0900

    Time: 21 ms (27.21%), Space: 27 MB (92.03%) - LeetHub

--- 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
+++ 1110-delete-nodes-and-return-forest/1110-delete-nodes-and-return-forest.cpp
@@ -16,21 +16,15 @@ public:
     void calculate(TreeNode* Node, TreeNode* U_Node, vector<int>& to_delete){
         bool consist_of_del = !!(std::count(to_delete.begin(), to_delete.end(), Node->val));
         
-        printf("Node : %d\n", Node->val);
-        printf("U_Node : %p\n", U_Node);
-        printf("consist_of_del : %b\n\n", consist_of_del);
-        
         if(!U_Node && !consist_of_del){
             ans.emplace_back(Node);
         }
         
         if(consist_of_del && U_Node){
             if(U_Node->left){
-                printf("U_Node->left : %d\n\n", U_Node->left->val);
                 if(U_Node->left->val == Node->val) U_Node->left = nullptr;
             }
             if(U_Node->right){
-                printf("U_Node->right : %d\n\n", U_Node->right->val);
                 if(U_Node->right->val == Node->val) U_Node->right = nullptr;
             }
         } 
