d5f3eeb4873f81b70b69ec359f08360bc4149c64
Oh jiwon <tinwoon21@gmail.com>
2026-08-08 15:29:27 +0900

    Time: 1220 ms (5.11%), Space: 17.1 MB (99.05%) - LeetHub

---
+++ 0085-maximal-rectangle/0085-maximal-rectangle.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int maximalRectangle(vector<vector<char>>& matrix) {
+        int ans = 0;
+        
+        for(int i=0; i < matrix.size(); i++){
+            for(int j = 0; j < matrix[0].size(); j++){
+                if(matrix[i][j] == '1'){
+                    int y = i, x = 0, max_col = INT_MAX;
+
+                    while((y < matrix.size()) && matrix[y][j] == '1'){
+                        x = j;
+                        while((x + 1 < matrix[0].size()) && matrix[y][x+1] == '1'){
+                            x++;
+                        }
+                        max_col = std::min(max_col, x - j + 1);
+                        ans = std::max(ans, max_col * (y++ - i + 1));
+                    }
+
+                    printf("max_col : %d, y : %d, i : %d\n", max_col, y, i);
+                }
+            }
+        }
+
+        return ans;
+    }
+};
